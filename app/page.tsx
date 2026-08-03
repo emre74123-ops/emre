cee65a7d2db3aa680fa48c44f9f43223ad7a256a
emre74123-ops <emre74123@gmail.com>
2026-08-03 16:45:22 +0300

    Profesyonel sayfa yonetimi yapisini ekle

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import HomeClient from "./HomeClient";
 import { readStoredSlides } from "../lib/slider-storage";
 import { readHeaderSettings } from "../lib/header-storage";
+import { readManagedPages } from "../lib/page-storage";
 
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
 export default async function Home() {
-  const [storedSlides, headerSettings] = await Promise.all([readStoredSlides(), readHeaderSettings()]);
+  const [storedSlides, headerSettings, managedPages] = await Promise.all([readStoredSlides(), readHeaderSettings(), readManagedPages()]);
   const slides = storedSlides.filter((slide) => slide.active);
-  return <HomeClient initialSlides={slides} headerSettings={headerSettings} />;
+  return <HomeClient initialSlides={slides} headerSettings={headerSettings} managedPages={managedPages} />;
 }
