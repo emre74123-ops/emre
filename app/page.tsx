eec8e85391a68c501e07c0e90da59b2c51002bcb
emre74123-ops <emre74123@gmail.com>
2026-08-03 13:36:25 +0300

    Header yonetim panelini ekle

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import HomeClient from "./HomeClient";
 import { readStoredSlides } from "../lib/slider-storage";
+import { readHeaderSettings } from "../lib/header-storage";
 
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
 export default async function Home() {
-  const slides = (await readStoredSlides()).filter((slide) => slide.active);
-  return <HomeClient initialSlides={slides} />;
+  const [storedSlides, headerSettings] = await Promise.all([readStoredSlides(), readHeaderSettings()]);
+  const slides = storedSlides.filter((slide) => slide.active);
+  return <HomeClient initialSlides={slides} headerSettings={headerSettings} />;
 }
 
